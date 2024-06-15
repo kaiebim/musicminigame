54eac517173cb7197d9ece0250da1b0d9c746d0e
kalebe bernabe <kalebesantosber@gmail.com>
2024-06-14 23:53:06 -0400

    sixth commit

--- musicas.js
+++ musicas.js
@@ -1,183 +1,55 @@
+let modelo = {
+    musica: "",
+    artista: "",
+    letra: [
+        [],
+        []
+    ]
+}
 
-let aguadebeber = {
-    musica: "Água de Beber",
-    artista: "Tom Jobim",
-    letra: [
-        "Eu quis amar, mas tive medo",
-        "E quis salvar meu coração",
-        "Mas o amor sabe um segredo",
-        "O medo pode matar o seu coração",
-        "Água de beber",
-        "Água de beber, camará",
-        "Água de beber",
-        "Água de beber, camará",
-        "Eu nunca fiz coisa tão certa",
-        "Entrei pra escola do perdão",
-        "A minha casa vive aberta",
-        "Abri todas as portas do coração",
-        "Água de beber",
-        "Água de beber, camará",
-        "Água de beber",
-        "Água de beber, camará",
-        "Eu sempre tive uma certeza",
-        "Que só me deu desilusão",
-        "É que o amor é uma tristeza",
-        "Muita mágoa, demais para um coração",
-        "Água de beber",
-        "Água de beber, camará",
-        "Água de beber",
-        "Água de beber, camará"
-    ]
-}
-let chovechuva = {
-    musica: "Chove Chuva",
-    artista: "Jorge Ben Jor",
-    letra: [
-        "Chove chuva",
-        "Chove sem parar",
-        "Chove chuva",
-        "Chove sem parar",
-        "Pois eu vou fazer uma prece",
-        "Pra Deus, nosso Senhor",
-        "Pra chuva parar",
-        "De molhar o meu divino amor",
-        "Que é muito lindo",
-        "É mais que o infinito",
-        "É puro e belo",
-        "Inocente como a flor",
-        "Por favor, chuva ruim",
-        "Não molhe mais",
-        "O meu amor assim",
-        "Por favor, chuva ruim",
-        "Não molhe mais",
-        "O meu amor assim",
-        "Chove chuva",
-        "Chove sem parar",
-        "Chove chuva",
-        "Chove sem parar",
-        "Pois eu vou fazer uma prece",
-        "Pra Deus, nosso Senhor",
-        "Pra chuva parar",
-        "De molhar o meu divino amor",
-        "Que é muito lindo",
-        "É mais que o infinito",
-        "É puro e belo",
-        "Inocente como a flor",
-        "Por favor, chuva ruim",
-        "Não molhe mais",
-        "O meu amor assim",
-        "Por favor, chuva ruim",
-        "Não molhe mais",
-        "O meu amor assim",
-        "Chove chuva",
-        "Chove sem parar",
-        "Chove chuva",
-        "Chove sem parar"
-    ]
-}
-let aguasdemarco = {
-    musica: "Águas de Março",
-    artista: "Tom Jobim",
-    letra: [
-        "É pau, é pedra, é o fim do caminho",
-        "É um resto de toco, é um pouco sozinho",
-        "É um caco de vidro, é a vida, é o Sol",
-        "É a noite, é a morte, é o laço, é o anzol",
-        "É peroba do campo, é o nó da madeira",
-        "Caingá, candeia, é o Matita Pereira",
-        "É madeira de vento, tombo da ribanceira",
-        "É o mistério profundo, é o queira ou não queira",
-        "É o vento ventando, é o fim da ladeira",
-        "É a viga, é o vão, festa da cumeeira",
-        "É a chuva chovendo, é conversa ribeira",
-        "Das águas de março, é o fim da canseira",
-        "É o pé, é o chão, é a marcha estradeira",
-        "Passarinho na mão, pedra de atiradeira",
-        "É uma ave no céu, é uma ave no chão",
-        "É um regato, é uma fonte, é um pedaço de pão",
-        "É o fundo do poço, é o fim do caminho",
-        "No rosto, o desgosto, é um pouco sozinho",
-        "É um estrepe, é um prego, é uma ponta, é um ponto",
-        "É um pingo pingando, é uma conta, é um conto",
-        "É um peixe, é um gesto, é uma prata brilhando",
-        "É a luz da manhã, é o tijolo chegando",
-        "É a lenha, é o dia, é o fim da picada",
-        "É a garrafa de cana, o estilhaço na estrada",
-        "É o projeto da casa, é o corpo na cama",
-        "É o carro enguiçado, é a lama, é a lama",
-        "É um passo, é uma ponte, é um sapo, é uma rã",
-        "É um resto de mato, na luz da manhã",
-        "São as águas de março fechando o verão",
-        "É a promessa de vida no teu coração",
-        "É uma cobra, é um pau, é João, é José",
-        "É um espinho na mão, é um corte no pé",
-        "São as águas de março fechando o verão",
-        "É a promessa de vida no teu coração",
-        "É pau, é pedra, é o fim do caminho",
-        "É um resto de toco, é um pouco sozinho",
-        "É um passo, é uma ponte, é um sapo, é uma rã",
-        "É um belo horizonte, é uma febre terçã",
-        "São as águas de março fechando o verão",
-        "É a promessa de vida no teu coração",
-        "Pau, edra, im, inho",
-        "Esto, oco, ouco, inho",
-        "Aco, idro, ida, ol, oite, orte, aço, zol",
-        "São as águas de março fechando o verão",
-        "É a promessa de vida no teu coração",
-    ]
-}
 let pelaluzdosolhosteus = {
     musica: "Pela Luz Dos Olhos Teus",
     artista: "Tom Jobim",
     letra: [
-        "Quando a luz dos olhos meus",
-        "E a luz dos olhos teus",
-        "Resolvem se encontrar",
-        "Ai, que bom que isso é, meu Deus",
-        "Que frio que me dá o encontro desse olhar",
-        "Mas se a luz dos olhos teus",
-        "Resiste aos olhos meus",
-        "Só pra me provocar",
-        "Meu amor, juro por Deus, me sinto incendiar",
-        "Meu amor, juro por Deus",
+        ["Meu amor, juro por Deus",
         "Que a luz dos olhos meus já não pode esperar",
         "Quero a luz dos olhos meus",
         "Na luz dos olhos teus",
-        "Sem mais lá-rá-rá-rá",
-        "Pela luz dos olhos teus",
-        "Eu acho, meu amor, que só se pode achar",
-        "Que a luz dos olhos meus precisa se casar",
-        "Quando a luz dos olhos meus",
+        "Sem mais lá-rá-rá-rá"],
+        ["Quando a luz dos olhos meus",
         "E a luz dos olhos teus",
         "Resolvem se encontrar",
-        "Ai, que bom que isso é, meu Deus",
-        "Que frio que me dá o encontro desse olhar",
-        "Mas se a luz dos olhos teus",
-        "Resiste aos olhos meus",
-        "Só pra me provocar",
-        "Meu amor, juro por Deus, me sinto incendiar (me sinto incendiar)",
-        "Meu amor, juro por Deus",
-        "Que a luz dos olhos meus já não pode esperar",
-        "Quero a luz dos olhos meus",
-        "Na luz dos olhos teus",
-        "Sem mais lá-rá-rá-rá",
-        "Pela luz dos olhos teus",
-        "Eu acho, meu amor, e só se pode achar",
-        "Que a luz dos olhos meus precisa se casar",
-        "Que a luz dos olhos meus precisa se casar",
-        "Que a luz dos olhos meus precisa se casar",
-        "Precisa se casar (precisa se casar)",
-        "Precisa se casar (precisa se casar)",
-        "Precisa se casar, precisa se casar",
-        "Precisa se casar, precisa se casar",
-        "Precisa se casar, precisa se casar",
-        "Precisa se casar, precisa se casar",
-        "Precisa se casar, precisa se casar",
-        "Precisa se casar, precisa se casar",
+        "Ai, que bom que isso é, meu Deus"]
+    ]
+}
+
+let chovechuva = {
+    musica: "Chove Chuva",
+    artista: "Jorge Ben Jor",
+    letra: [
+        ["Pois eu vou fazer uma prece",
+        "Pra Deus, nosso Senhor",
+        "Pra chuva parar",
+        "De molhar o meu divino amor"]
+    ]
+}
+
+let abanda = {
+    musica: "A Banda",
+    artista: "Chico Buarque",
+    letra: [
+        ["O velho fraco se esqueceu do cansaço e pensou",
+        "Que ainda era moço pra sair no terraço e dançou",
+        "A moça feia debruçou na janela",
+        "Pensando que a banda tocava pra ela"],
+        ["O homem sério que contava dinheiro parou",
+        "O faroleiro que contava vantagem parou",
+        "A namorada que contava as estrelas",
+        "Parou para ver, ouvir e dar passagem"]
     ]
 }
 
 export const letras = [
-    aguadebeber,chovechuva,aguasdemarco,pelaluzdosolhosteus
+    pelaluzdosolhosteus,chovechuva,abanda
 ];
 
--- script.js
+++ script.js
@@ -1,73 +1,90 @@
-import { letras } from './musicas.js';
+import { letras } from "./musicas.js";
+let QualUsar = Math.floor(Math.random() * letras.length);
+let QualParte = Math.floor(Math.random() * letras[QualUsar].letra.length);
+let attempt = 0;
+let coracoes = ['&#10084;','&#10084;','&#10084;','&#10084;','&#10084;'];
 
-let datetime = Math.floor(new Date().getTime() / (1000 * 60 * 60 * 24)) - 19868;
-let insira = document.getElementById("nome");
-let button = document.getElementById("button");
-let musica = document.getElementById("musica").innerHTML = letras[datetime].musica;
-let artista = document.getElementById("artista").innerHTML = letras[datetime].artista;
+let v1 = document.getElementById("v1"),
+    v2 = document.getElementById("v2"),
+    v3 = document.getElementById("v3"),
+    v4 = document.getElementById("v4"),
+    v5 = document.getElementById("v5");
 
-insira.addEventListener("keypress", function(event) {
-    if ((event.key === "Enter") && (insira.value !== '')) {
-        event.preventDefault();
-        pog();
+let t1 = document.getElementById("t1"),
+    t2 = document.getElementById("t2"),
+    t3 = document.getElementById("t3"),
+    t4 = document.getElementById("t4"),
+    t5 = document.getElementById("t5");
+
+v1.innerText = letras[QualUsar].letra[QualParte][0];
+
+let input = document.getElementById("nome");
+input.addEventListener("keypress",function(event){
+    if ((event.key === "Enter")&&(input.value !== '')){
+        inputed();
+        input.value = '';
     }
+})
+
+function inputed(){
+    let responseInput = input.value.toLocaleLowerCase().replace(/[\s,-\/]/g,'').replace(/[ãáàâä]/g,"a").replace(/[éêèë]/g,"e").replace(/[íìîï]/g,"i").replace(/[ôõóòö]/g,"o").replace(/[ûúùü]/g,"u");
+    let responseMusica = letras[QualUsar].musica.toLocaleLowerCase().replace(/[\s,-\/]/g,'').replace(/[ãáàâä]/g,"a").replace(/[éêèë]/g,"e").replace(/[íìîï]/g,"i").replace(/[ôõóòö]/g,"o").replace(/[ûúùü]/g,"u");
+    console.log(responseInput,responseMusica);
+    attempt += 1
+    console.log(attempt)
+    if (responseInput === responseMusica)
+        window.alert("ACERTOUOUOUOUOU")
+    else{
+        if (attempt < 5){
+        coracoes[attempt - 1] = ''
+        document.getElementById("hearts").innerHTML = (coracoes[0]+coracoes[1]+coracoes[2]+coracoes[3]+coracoes[4])
+        }
+        else
+            document.getElementById("hearts").innerHTML = "VOCÊ PERDEU";
+    }
+    switch (attempt){
+        case 1:
+            t1.innerHTML = input.value;
+            t1.scrollIntoView({ behavior: "smooth", block: "start" });
+            v2.classList.remove("nonvisible");
+            v2.innerText = letras[QualUsar].letra[QualParte][1];
+            break;
+        case 2:
+            t2.innerHTML = input.value;
+            t2.scrollIntoView({ behavior: "smooth", block: "start" });
+            v3.classList.remove("nonvisible");
+            v3.innerText = letras[QualUsar].letra[QualParte][2];
+            break;
+        case 3:
+            t3.innerHTML = input.value;
+            t3.scrollIntoView({ behavior: "smooth", block: "start" });
+            v4.classList.remove("nonvisible");
+            v4.innerText = letras[QualUsar].letra[QualParte][3];
+            break;
+        case 4:
+            t4.innerHTML = input.value;
+            t4.scrollIntoView({ behavior: "smooth", block: "start" });
+            v5.classList.remove("nonvisible");
+            v5.innerText = letras[QualUsar].artista;
+            break;
+        case 5:
+            t5.scrollIntoView({ behavior: "smooth", block: "start" });
+            t5.innerHTML = input.value;
+            break;
+    }
+}
+
+window.onload = function() {
+    exibirPopup();
+};
+function exibirPopup() {
+    let popup = document.getElementById('popup');
+    popup.style.display = 'block';
+}
+
+let btnOk = document.querySelector('#popup button');
+btnOk.addEventListener('click', function() {
+    let popup = document.getElementById('popup');
+    popup.style.display = 'none';
 });
 
-let a = 0;
-let strings = [];
-let certa = letras[datetime].letra;
-
-
-
-function pog() {
-    console.log(datetime);
-    
-    let certo = document.getElementById("certo");
-    
-    let real = document.createElement("p");
-    insira.placeholder = `Insira o ${a + 1}° verso da música`;
-    strings[a] = insira.value;
-    real.innerHTML = certa[a];
-    checar(a, certa);
-    a++;
-    insira.value = '';
-    
-    certo.appendChild(real);
-}
-
-function checar(n, r) {
-    let container = document.getElementById("inputs");
-    let p = document.createElement("p");
-    let str1 = strings[n].toLowerCase().replace(/[\s,!?.;:*)(]/g, '').replace(/[ãáàâ]/g, "a").replace(/[èéê]/g, "e").replace(/[íì]/g, "i").replace(/[õôóò]/g, "o").replace(/[úùû]/g, "u");
-    let str2 = r[n].toLowerCase().replace(/[\s,!?.;:*)(]/g, '').replace(/[ãáàâ]/g, "a").replace(/[èéê]/g, "e").replace(/[íì]/g, "i").replace(/[õôóò]/g, "o").replace(/[úùû]/g, "u");
-    let differences = findDifferences(str1, str2);
-    
-    console.log(str1);
-    console.log(str2);
-
-
-    if (differences.length > 0) {
-        p.classList.add("errou");
-    } else {
-        p.classList.add("acertou");
-    }
-    
-    p.innerHTML = insira.value;
-    container.appendChild(p);
-}
-
-function findDifferences(str1, str2) {
-    let differences = [];
-    let length = Math.max(str1.length, str2.length);
-
-    for (let i = 0; i < length; i++) {
-        if (str1[i] !== str2[i]) {
-            if (str1[i] !== undefined) differences.push(str1[i]);
-            if (str2[i] !== undefined) differences.push(str2[i]);
-        }
-    }
-
-    return differences;
-}
-
-window.pog = pog;
